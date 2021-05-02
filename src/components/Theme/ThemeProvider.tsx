c9533eba4dcf63fe3ac37b13a20248bea6ff156c
MATEUSZ RYCHLIK <mathtev@gmail.com>
2021-05-03 01:03:32 +0200

    first commit

---
+++ src/components/Theme/ThemeProvider.tsx
@@ -0,0 +1,47 @@
+import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
+import React, { createContext, useState } from 'react';
+import createTheme from '../../theme/theme';
+
+interface MyThemeContext {
+  isDark: boolean;
+  toggleTheme: () => void;
+}
+
+interface ThemeProviderProps {
+  defaultDark: boolean;
+}
+
+export const ThemeContext = createContext<MyThemeContext>({
+  isDark: false,
+  toggleTheme: () => undefined,
+});
+
+const ThemeProvider: React.FC<ThemeProviderProps> = ({
+  defaultDark,
+  children,
+}) => {
+  const [isDark, setDark] = useState(defaultDark);
+
+  const toggleTheme = () => {
+    setDark(!isDark);
+    localStorage.setItem('theme', (!isDark).toString());
+  };
+
+  const theme = createTheme();
+
+  return (
+    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </MuiThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+ThemeProvider.defaultProps = {
+  defaultDark: false,
+};
+
+
+export default ThemeProvider;
---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+import { createMuiTheme, Theme } from "@material-ui/core/styles";
+
+const createTheme = (): Theme => createMuiTheme({
+  // overrides {
+
+  // },
+  palette: {
+    // info: {
+    //   main: "#caabaa"
+    // }
+  }
+})
+
+export default createTheme;
