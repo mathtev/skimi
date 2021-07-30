7707e3eb778c610663c60bb3d2cb66e37983bd42
MATEUSZ RYCHLIK <mathtev@gmail.com>
2021-07-30 20:24:48 +0200

    set up server

---
+++ Backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+const schema = require('./schema.ts');
+const app = express();
+
+app.use(cors());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
